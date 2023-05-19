67d44200919fe3ff54539c438e4247e393b9a58a
Your Name <you@example.com>
2023-05-19 14:13:29 +0400

    mopped up 2 bzik

--- parse/check_fc.c
+++ parse/check_fc.c
@@ -1,10 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_fc.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tyenokya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/19 14:12:52 by tyenokya          #+#    #+#             */
+/*   Updated: 2023/05/19 14:12:54 by tyenokya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
 static void	check_rgb_info(char **info, t_flag *fl)
 {
 	while (info && info[(*fl).i] && info[(*fl).i][(*fl).j])
 	{
-		printf("%s", info[(*fl).i]);
 		if (info[(*fl).i][(*fl).j] == 'F')
 		{
 			(*fl).flag0++;
@@ -29,12 +40,10 @@ void	check_fc(char **info)
 	fl.flag1 = 0;
 	while (info && info[fl.i] && fl.i < 6)
 	{
+		fl.j = 0;
 		check_rgb_info(info, &fl);
 		++fl.i;
 	}
 	if (fl.flag0 != 1 || fl.flag1 != 1)
-	{
-		printf("hello %d %d\n", fl.flag0, fl.flag1);
 		exit_false_text_param();
-	}
 }
